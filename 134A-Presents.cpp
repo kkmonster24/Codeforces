5e62f6fb2e3ff05e8a159348eeaf9a75acc61c20
kkmonster24 <45737735+kkmonster24@users.noreply.github.com>
2019-04-27 18:00:35 +0530

    Add files via upload

---
+++ 134A-Presents.cpp
@@ -0,0 +1,21 @@
+//http://codeforces.com/problemset/problem/136/A
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	pair<int,int> array[n];
+	for(int i=0;i<n;i++){
+		int data;
+		cin>>data;
+		array[i]=make_pair(data,i);
+	}
+	sort(array,array+n);
+	for(int i=0;i<n;i++){
+		cout<<array[i].second+1<<" ";
+	}
+
+}
